9f9e0b2596d5a801d0ae69ba73f719bb17cb45a2
Catherine Cordova <ccordova41@gmail.com>
2017-11-22 03:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 $(document).ready(function (){
-  $.get("sentence.html", function(response) {
-    // Here we are getting the element on the page with the id of sentences and
-    // inserting the response
-    $("#sentences").html(response);
-  });
 });
